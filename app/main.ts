1c742e1b27a8c054fbacd7e3e1e8d7fe84d5444c
luftkluft <luftkluft@gmail.com>
2021-10-24 03:25:56 +0300

    TScript eslint prettier

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  mainWindow.loadFile('app/index.html')
+}
+
+app.on('ready', createWindow)
