678b840aafe56a380ac2f45487d34310dee27833
luftkluft <luftkluft@gmail.com>
2021-10-25 23:57:06 +0300

    Use ejs

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+let electronEjs = require('electron-ejs')
+
+let ejs = new electronEjs({ name: 'Luft Kluft!' }, {})
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   })
-  mainWindow.loadFile('app/index.html')
+  mainWindow.loadFile('app/views/index.ejs')
 }
 
 app.on('ready', createWindow)
