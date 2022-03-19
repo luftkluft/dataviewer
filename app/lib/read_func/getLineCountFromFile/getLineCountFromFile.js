269469a6d5ce09fb2f434dda8e24bc92dda00525
luftkluft <luftkluft@gmail.com>
2022-03-19 15:28:15 +0200

    Dev: run jest

--- app/lib/read_func/getLineCountFromFile/getLineCountFromFile.js
+++ app/lib/read_func/getLineCountFromFile/getLineCountFromFile.js
@@ -1,2 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getLineCountFromFile = void 0;
+function getLineCountFromFile() {
+    return 5;
+}
+exports.getLineCountFromFile = getLineCountFromFile;
 //# sourceMappingURL=getLineCountFromFile.js.map
---
+++ app/lib/read_func/getLineCountFromFile/getLineCountFromFile.ts
@@ -0,0 +1,3 @@
+export function getLineCountFromFile() {
+  return 5 // TODO
+}
