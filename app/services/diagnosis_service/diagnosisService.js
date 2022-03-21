217750514c62829318136dfb6996c32368835398
luftkluft <luftkluft@gmail.com>
2022-03-21 14:29:54 +0200

    Dev: create diagnosisService tree

---
+++ app/services/diagnosis_service/diagnosisService.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DS = void 0;
+var DS = (function () {
+    function DS() {
+    }
+    return DS;
+}());
+exports.DS = DS;
+//# sourceMappingURL=diagnosisService.js.map
---
+++ app/services/diagnosis_service/diagnosisService.ts
@@ -0,0 +1,3 @@
+export class DS {
+  // TODO
+}
