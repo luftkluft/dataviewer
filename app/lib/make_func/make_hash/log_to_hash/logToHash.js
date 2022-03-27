d84be87b48010868ba33f7b18047a473122e51c8
luftkluft <luftkluft@gmail.com>
2022-03-28 01:03:57 +0300

    Dev: compressHashBody()

--- app/lib/make_func/make_hash/log_to_hash/logToHash.js
+++ app/lib/make_func/make_hash/log_to_hash/logToHash.js
@@ -6,6 +6,7 @@ var dialog = require('electron').dialog;
 var makeHeaderTable_1 = require("../../make_csv/make_header_table/makeHeaderTable");
 var makeBodyTable_1 = require("../../make_csv/make_body_table/makeBodyTable");
 var hashing_1 = require("../hashing/hashing");
+var rws_1 = require("../../../../services/read_write_service/rws");
 var createFileName = function () {
     var current = new Date();
     var datePart = (new Date().toString()).split(/\.|\s|:/);
@@ -13,7 +14,34 @@ var createFileName = function () {
     fileName = datePart[3] + "-" + datePart[1] + "-" + datePart[2] + "-" + datePart[4] + "-" + datePart[5] + ".csv";
     return fileName;
 };
+var compressHashBody = function (sHashBody, deep) {
+    try {
+        var endLine = "\n";
+        var compressBody = '\n';
+        var currentLine = '';
+        var sLinesArray = [];
+        sLinesArray = sHashBody.split(endLine);
+        var i = 0;
+        for (i = 0; i < sLinesArray.length - deep; i++) {
+            if (rws_1.RWS.readDataLineFromLog(sLinesArray[i]) == currentLine) {
+                continue;
+            }
+            else {
+                if (rws_1.RWS.readDataLineFromLog(sLinesArray[i]).length) {
+                    compressBody += rws_1.RWS.readTimeFromLog(sLinesArray[i]) + rws_1.RWS.readDataLineFromLog(sLinesArray[i]);
+                    compressBody += '\n';
+                    currentLine = rws_1.RWS.readDataLineFromLog(sLinesArray[i]);
+                }
+            }
+        }
+        return compressBody;
+    }
+    catch (error) {
+        return '';
+    }
+};
 function logToHash(logFile, variablesListFile, csvFileSavePath, logParams, deep) {
+    if (deep === void 0) { deep = 1; }
     try {
         var sData = '';
         var sHash = '';
@@ -22,7 +50,10 @@ function logToHash(logFile, variablesListFile, csvFileSavePath, logParams, deep)
         var file = savePath + fileName;
         sHash += (0, makeHeaderTable_1.makeHeaderTable)(variablesListFile, logParams.delemiter);
         sData = (0, hashing_1.hashing)((0, makeBodyTable_1.makeBodyTable)(logFile, variablesListFile, logParams.delemiter), deep);
+        sData = compressHashBody(sData, deep);
         sHash += sData;
+        console.log('###############');
+        console.log(sHash);
         return sHash;
     }
     catch (error) {
--- app/lib/make_func/make_hash/log_to_hash/logToHash.ts
+++ app/lib/make_func/make_hash/log_to_hash/logToHash.ts
@@ -3,6 +3,7 @@ const { dialog } = require('electron')
 import { makeHeaderTable } from '../../make_csv/make_header_table/makeHeaderTable'
 import { makeBodyTable } from '../../make_csv/make_body_table/makeBodyTable'
 import { hashing } from '../hashing/hashing'
+import { RWS } from '../../../../services/read_write_service/rws'
 
 const createFileName = () => {
   let current = new Date()
@@ -12,7 +13,35 @@ const createFileName = () => {
   return fileName
 }
 
-export function logToHash(logFile: string, variablesListFile: string, csvFileSavePath: string, logParams: any, deep: number) {
+const compressHashBody = (sHashBody: string, deep: number) => {
+  try {
+    const endLine: string = "\n"
+    let compressBody: string = '\n'
+    let currentLine: string = ''
+    let sLinesArray: string[] = []
+    sLinesArray = sHashBody.split(endLine)
+    let i: number = 0
+    
+    for (i = 0; i < sLinesArray.length - deep; i++) {
+      if ( RWS.readDataLineFromLog(sLinesArray[i]) == currentLine) {
+        continue
+      }
+      else {
+        if (RWS.readDataLineFromLog(sLinesArray[i]).length) {
+          compressBody +=  RWS.readTimeFromLog(sLinesArray[i]) + RWS.readDataLineFromLog(sLinesArray[i])
+          compressBody += '\n'
+          currentLine = RWS.readDataLineFromLog(sLinesArray[i])
+        }
+      }
+    }
+    return compressBody
+  } catch (error) {
+    return ''
+  }
+
+}
+
+export function logToHash(logFile: string, variablesListFile: string, csvFileSavePath: string, logParams: any, deep: number = 1) {
   try {
     let sData: string = ''
     let sHash: string = ''
@@ -21,7 +50,10 @@ export function logToHash(logFile: string, variablesListFile: string, csvFileSav
     let file: string = savePath + fileName
     sHash += makeHeaderTable(variablesListFile, logParams.delemiter)
     sData = hashing(makeBodyTable(logFile, variablesListFile, logParams.delemiter), deep)
+    sData = compressHashBody(sData, deep)
     sHash += sData
+    console.log('###############')
+    console.log(sHash)
     return sHash
   } catch (error) {
     return error
