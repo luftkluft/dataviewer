112969a7217d8762f21f29bb55b0512c9ef23145
luftkluft <luftkluft@gmail.com>
2022-03-28 17:43:39 +0300

    Dev: new diagnosis field for var_ist

--- app/views/diagnostics/diagnostics.js
+++ app/views/diagnostics/diagnostics.js
@@ -11,6 +11,8 @@ var patternFileField = formDiagn.querySelector('.choose-pattern-file-field');
 patternFileField.value = ipcDiagnRenderer.sendSync('last_opened_pattern_file');
 var errorFileField = formDiagn.querySelector('.choose-error-file-field');
 errorFileField.value = ipcDiagnRenderer.sendSync('last_opened_error_file');
+var vListFileField = formDiagn.querySelector('.choose-variable-list-file-field');
+vListFileField.value = ipcDiagnRenderer.sendSync('last_opened_variable_list_file');
 var deepField = formDiagn.querySelector('.deep-field');
 deepField.value = ipcDiagnRenderer.sendSync('get_deep_test');
 var fileStat = function (stat) {
@@ -54,6 +56,25 @@ var chooseErrorFile = function () {
         });
     }
 };
+var chooseVariableListFile = function () {
+    var path = ipcDiagnRenderer.sendSync('open-variable-list-file-dialog');
+    var memo = document.getElementById('memo');
+    if (path == 'no_file_selected') {
+        memo.value = ipcDiagnRenderer.sendSync('i18n', path);
+    }
+    else {
+        vListFileField.value = path;
+        fs.stat(path, function (err, stat) {
+            if (err) {
+                memo.value = err;
+            }
+            else {
+                memo.value = fileStat(stat);
+                setMakeDiagnosticsButtonStatus();
+            }
+        });
+    }
+};
 formDiagn.addEventListener('submit', function (event) {
     event.preventDefault();
     var memo = document.getElementById('memo');
@@ -64,6 +85,7 @@ var setMakeDiagnosticsButtonStatus = function () {
     var makeDiagnosticButton = document.querySelector('.make-diagnostics-btn');
     if (patternFileField.value == undefined || patternFileField.value.length == 0
         || errorFileField.value == undefined || errorFileField.value.length == 0
+        || vListFileField.value == undefined || vListFileField.value.length == 0
         || deepField.value == undefined || deepField.value <= 0) {
         makeDiagnosticButton.disabled = true;
     }
--- app/views/diagnostics/diagnostics.ts
+++ app/views/diagnostics/diagnostics.ts
@@ -6,6 +6,8 @@ const patternFileField = formDiagn.querySelector('.choose-pattern-file-field')
 patternFileField.value = ipcDiagnRenderer.sendSync('last_opened_pattern_file')
 const errorFileField = formDiagn.querySelector('.choose-error-file-field')
 errorFileField.value = ipcDiagnRenderer.sendSync('last_opened_error_file')
+const vListFileField = formDiagn.querySelector('.choose-variable-list-file-field')
+vListFileField.value = ipcDiagnRenderer.sendSync('last_opened_variable_list_file')
 const deepField = formDiagn.querySelector('.deep-field')
 deepField.value = ipcDiagnRenderer.sendSync('get_deep_test')
 
@@ -49,6 +51,24 @@ const chooseErrorFile = () => {
   }
 }
 
+const chooseVariableListFile = () => {
+  const path = ipcDiagnRenderer.sendSync('open-variable-list-file-dialog')
+  const memo: any = document.getElementById('memo')
+  if (path == 'no_file_selected') {
+    memo.value = ipcDiagnRenderer.sendSync('i18n', path)
+  } else {
+    vListFileField.value = path
+    fs.stat(path, function (err: any, stat: any) {
+      if (err) {
+        memo.value = err
+      } else {
+        memo.value = fileStat(stat)
+        setMakeDiagnosticsButtonStatus()
+      }
+    })
+  }
+}
+
 formDiagn.addEventListener('submit', function (event: any) {
   event.preventDefault()
   const memo: any = document.getElementById('memo')
@@ -60,6 +80,7 @@ const setMakeDiagnosticsButtonStatus = () => {
   const makeDiagnosticButton: any = document.querySelector('.make-diagnostics-btn')
   if (patternFileField.value == undefined || patternFileField.value.length == 0
     || errorFileField.value == undefined || errorFileField.value.length == 0
+    || vListFileField.value == undefined || vListFileField.value.length == 0
     || deepField.value == undefined || deepField.value <= 0) {
     makeDiagnosticButton.disabled = true
   } else {
