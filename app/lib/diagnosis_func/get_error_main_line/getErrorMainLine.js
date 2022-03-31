b0ad072d2da82564ae8d2bb0039ff8584197500e
luftkluft <luftkluft@gmail.com>
2022-03-31 23:26:05 +0300

    BugFix: getErrorMineLines()

--- app/lib/diagnosis_func/get_error_main_line/getErrorMainLine.js
+++ app/lib/diagnosis_func/get_error_main_line/getErrorMainLine.js
@@ -3,43 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getErrorMainLine = void 0;
 var fs = require('fs');
 var hashing_1 = require("../../make_func/make_hash/hashing/hashing");
-var rws_1 = require("../../../services/read_write_service/rws");
-var strHexToBinFromSiemens_1 = require("../../a2b/strHexToBinFromSiemens/strHexToBinFromSiemens");
-var deleteSeparator_1 = require("../../make_func/make_parser/delete_separator/deleteSeparator");
-var headerInfo_1 = require("../../make_func/make_parser/header_info/headerInfo");
-var getErrorLog = function (sLastOpenedErrorFile) {
-    if (sLastOpenedErrorFile === void 0) { sLastOpenedErrorFile = ''; }
-    try {
-        var sResult = '';
-        if (fs.existsSync(sLastOpenedErrorFile)) {
-            sResult = fs.readFileSync(sLastOpenedErrorFile, 'utf8');
-            return sResult;
-        }
-        else {
-            return '';
-        }
-        return sResult;
-    }
-    catch (error) {
-        return '';
-    }
-};
-var getBodyErrorLog = function (sErrorLog) {
-    try {
-        var endLine = '\n';
-        var sResult = '';
-        var linesArray = [];
-        linesArray = sErrorLog.split(endLine);
-        var i = 0;
-        for (i = 1; i < linesArray.length - 1; i++) {
-            sResult += linesArray[i] + endLine;
-        }
-        return sResult;
-    }
-    catch (error) {
-        return '';
-    }
-};
+var makeBodyTable_1 = require("../../make_func/make_csv/make_body_table/makeBodyTable");
 var getLastLogLine = function (sLines) {
     if (sLines === void 0) { sLines = ''; }
     try {
@@ -54,90 +18,11 @@ var getLastLogLine = function (sLines) {
         return '';
     }
 };
-var makeErrorBodyTable = function (sBodyErrorLog, variablesListFile, separator) {
-    var sVariableListLines = "";
-    var sLogLines = "";
-    var sDataLine = "";
-    var sLine = "";
-    var sBitsLine = "";
-    var sReturn = "";
-    var isFirstStringRemoved = false;
-    var sumCharCount = 0;
-    if (fs.existsSync(variablesListFile)) {
-        sVariableListLines = fs.readFileSync(variablesListFile, 'utf8');
-    }
-    else {
-        return '';
-    }
-    sVariableListLines = (0, deleteSeparator_1.deleteSeparator)(sVariableListLines, "\r");
-    var sHeaderInfo = (0, headerInfo_1.headerInfo)(sVariableListLines, separator);
-    if (sBodyErrorLog.length) {
-        sLogLines = sBodyErrorLog;
-    }
-    else {
-        return '';
-    }
-    sLogLines = (0, deleteSeparator_1.deleteSeparator)(sLogLines, "\r");
-    var j = 0;
-    for (j = 0; j < sLogLines.length; j++) {
-        if (sLogLines[j] == '\n') {
-            isFirstStringRemoved = true;
-        }
-        if (sLogLines[j] != '\n') {
-            if (isFirstStringRemoved) {
-                sLine += sLogLines[j];
-            }
-        }
-        else {
-            sDataLine = rws_1.RWS.readDataLineFromLog(sLine);
-            sBitsLine = (0, strHexToBinFromSiemens_1.strHexToBinFromSiemens)(sDataLine);
-            if (sBitsLine != "") {
-                sReturn += rws_1.RWS.readTimeFromLog(sLine);
-                sReturn += separator;
-                var wordCharCount = 0;
-                var k = 0;
-                for (k = 0; k < sBitsLine.length; k++) {
-                    if (sBitsLine[k] == '+' || sBitsLine[k] == '-') {
-                        wordCharCount++;
-                    }
-                    if (wordCharCount) {
-                        if (wordCharCount >= 6) {
-                            sReturn += sBitsLine[k];
-                            sReturn += separator;
-                            wordCharCount = 0;
-                            continue;
-                        }
-                        else {
-                            sReturn += sBitsLine[k];
-                            wordCharCount++;
-                            sumCharCount++;
-                            continue;
-                        }
-                    }
-                    if (!wordCharCount)
-                        if (sHeaderInfo[k - sumCharCount]) {
-                            sReturn += sBitsLine[k];
-                            sReturn += separator;
-                        }
-                }
-            }
-            sReturn += '\n';
-            sLine = "";
-            sBitsLine = "";
-            sDataLine = "";
-            sumCharCount = 0;
-        }
-    }
-    return sReturn;
-};
 function getErrorMainLine(sLastOpenedErrorFile, variablesListFile, separator, deep) {
     if (deep === void 0) { deep = 1; }
     try {
         var sResult = '';
-        var sErrorLog = '';
-        sErrorLog = getErrorLog(sLastOpenedErrorFile);
-        sResult = getBodyErrorLog(sErrorLog);
-        sResult = makeErrorBodyTable(sResult, variablesListFile, separator);
+        sResult = (0, makeBodyTable_1.makeBodyTable)(sLastOpenedErrorFile, variablesListFile, separator);
         sResult = (0, hashing_1.hashing)(sResult, deep);
         sResult = getLastLogLine(sResult);
         return sResult;
--- app/lib/diagnosis_func/get_error_main_line/getErrorMainLine.ts
+++ app/lib/diagnosis_func/get_error_main_line/getErrorMainLine.ts
@@ -1,40 +1,6 @@
 let fs = require('fs')
 import { hashing } from '../../make_func/make_hash/hashing/hashing'
-import { RWS } from '../../../services/read_write_service/rws'
-import { strHexToBinFromSiemens } from '../../a2b/strHexToBinFromSiemens/strHexToBinFromSiemens'
-import { deleteSeparator } from '../../make_func/make_parser/delete_separator/deleteSeparator'
-import { headerInfo } from '../../make_func/make_parser/header_info/headerInfo'
-
-const getErrorLog = (sLastOpenedErrorFile: string = '') => {
-  try {
-    let sResult: string = ''
-    if (fs.existsSync(sLastOpenedErrorFile)) {
-      sResult = fs.readFileSync(sLastOpenedErrorFile, 'utf8')
-      return sResult
-    } else {
-      return ''
-    }
-    return sResult
-  } catch (error) {
-    return ''
-  }
-}
-
-const getBodyErrorLog = (sErrorLog: string) => {
-  try {
-    const endLine = '\n'
-    let sResult: string = ''
-    let linesArray: string[] = []
-    linesArray = sErrorLog.split(endLine)
-    let i: number = 0
-    for (i = 1; i < linesArray.length - 1; i++) {
-      sResult += linesArray[i] + endLine
-    }
-    return sResult
-  } catch (error) {
-    return ''
-  }
-}
+import { makeBodyTable } from '../../make_func/make_csv/make_body_table/makeBodyTable'
 
 const getLastLogLine = (sLines: string = '') => {
   try {
@@ -49,91 +15,10 @@ const getLastLogLine = (sLines: string = '') => {
   }
 }
 
-const makeErrorBodyTable = (sBodyErrorLog: string, variablesListFile: string, separator: string) => {
-  let sVariableListLines: string = ""
-  let sLogLines: string = ""
-  let sDataLine: string = ""
-  let sLine: string = ""
-  let sBitsLine: string = ""
-  let sReturn: string = ""
-  let isFirstStringRemoved: boolean = false
-  let sumCharCount: number = 0
-
-  if (fs.existsSync(variablesListFile)) {
-    sVariableListLines = fs.readFileSync(variablesListFile, 'utf8')
-  } else {
-    return ''
-  }
-  sVariableListLines = deleteSeparator(sVariableListLines, "\r")
-
-  const sHeaderInfo: string = headerInfo(sVariableListLines, separator)
-
-  if (sBodyErrorLog.length) {
-    sLogLines = sBodyErrorLog
-  } else {
-    return ''
-  }
-  sLogLines = deleteSeparator(sLogLines, "\r")
-  let j: number = 0
-  for (j = 0; j < sLogLines.length; j++) {
-    if (sLogLines[j] == '\n') {
-      isFirstStringRemoved = true
-    }
-    if (sLogLines[j] != '\n') {
-      if (isFirstStringRemoved) {
-        sLine += sLogLines[j]
-      }
-    }
-    else {
-      sDataLine = RWS.readDataLineFromLog(sLine)
-      sBitsLine = strHexToBinFromSiemens(sDataLine)
-      if (sBitsLine != "") {
-        sReturn += RWS.readTimeFromLog(sLine)
-        sReturn += separator
-        let wordCharCount: number = 0
-        let k: number = 0
-        for (k = 0; k < sBitsLine.length; k++) {
-          if (sBitsLine[k] == '+' || sBitsLine[k] == '-') {
-            wordCharCount++
-          }
-          if (wordCharCount) {
-            if (wordCharCount >= 6) {
-              sReturn += sBitsLine[k]
-              sReturn += separator
-              wordCharCount = 0
-              continue
-            }
-            else {
-              sReturn += sBitsLine[k]
-              wordCharCount++
-              sumCharCount++
-              continue
-            }
-          }
-          if (!wordCharCount)
-            if (sHeaderInfo[k - sumCharCount]) {
-              sReturn += sBitsLine[k]
-              sReturn += separator
-            }
-        }
-      }
-      sReturn += '\n'
-      sLine = ""
-      sBitsLine = ""
-      sDataLine = ""
-      sumCharCount = 0
-    }
-  }
-  return sReturn
-}
-
 export function getErrorMainLine(sLastOpenedErrorFile: string, variablesListFile: string, separator: string, deep: number = 1) {
   try {
     let sResult: string = ''
-    let sErrorLog: string = ''
-    sErrorLog = getErrorLog(sLastOpenedErrorFile)
-    sResult = getBodyErrorLog(sErrorLog)
-    sResult = makeErrorBodyTable(sResult, variablesListFile, separator)
+    sResult = makeBodyTable(sLastOpenedErrorFile, variablesListFile, separator)
     sResult = hashing(sResult, deep)
     sResult = getLastLogLine(sResult)
     return sResult
